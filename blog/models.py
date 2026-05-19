bd13bef505a14f8dfe0093ad543ec5a9c2f1be0f
na <rizeyusi@gmail.com>
2026-05-19 18:07:22 +0900

    Add files via upload

--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,11 @@
-from typing import TYPE_CHECKING
-
 from django.db import models
 from django.contrib.auth.models import User
 import os
-
-if TYPE_CHECKING:
-    from django.db.models.manager import Manager
 
 # Create your models here.
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
-
-    if TYPE_CHECKING:
-        post_set: Manager["Post"]
 
     def __str__(self):
         return self.name
@@ -25,9 +17,6 @@ class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
 
-    if TYPE_CHECKING:
-        post_set: Manager["Post"]
-
     def __str__(self):
         return self.name
 
@@ -35,7 +24,7 @@ class Category(models.Model):
         return f'/blog/category/{self.slug}/'
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'categories'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -44,12 +33,14 @@ class Post(models.Model):
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
-    tags = models.ManyToManyField(Tag, blank=True)    
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
@@ -57,14 +48,18 @@ class Post(models.Model):
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
 
-    def get_file_name(self) -> str:
-        name = self.file_upload.name
-        if not name:
-            return ''
-        return os.path.basename(name)
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
 
-    def get_file_ext(self) -> str:
-        file_name = self.get_file_name()
-        if not file_name:
-            return ''
-        return file_name.rsplit('.', 1)[-1]
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
