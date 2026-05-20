4950cf481d3f5cd6e007ac8e5537d0ef20830737
na <rizeyusi@gmail.com>
2026-05-20 18:19:14 +0900

    Add files via upload

--- blog/models.py
+++ blog/models.py
@@ -63,3 +63,9 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'{self.author}::{self.content}'
+
+    def get_absolute_url(self):
+        return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avartar_url(self):
+        return 'http://placehold.it/50x50'
